07462ec7ea6fd1f4e22c5d7e15a1a68ce0d7f36a
heatherzen <heather.zen@gmail.com>
2020-06-14 11:06:46 -0700

    testing for loops

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-//var pwCriteria = window.alert("Please choose your password criteria.");
+var pwCriteria = window.alert("Please choose your password criteria.");
 var pwLength = 8 <= 128
 var pwUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var pwLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -9,7 +9,7 @@ var specialCharacters = ["U+0021", "U+0023", "U+0024", "U+0025", "U+0026", "U+00
 console.log(pwLength, pwUppercase, pwLowercase, numericValue, specialCharacters);
 
 var generateBtn = function() {
-  while () {
+  for () {
     //password length criteria
     var pwLength = function() {
       window.prompt("Choose the length of your password. (It must be a minimum of 8 charactors and a maximum of 128 characters in length).");
@@ -24,7 +24,7 @@ var generateBtn = function() {
 
     //verify Uppercase 'yes' or 'no'
     var pwUppercase = function() {
-      for (var i = 0; i < pwUppercase.length; i+(i+25)) {
+      //for (var i = 0; i < pwUppercase.length; i+(i+25)) {
         window.prompt("Would you like to include Upper case letters? Type 'Yes' or 'No'");
         if (pwUppercase === "Yes" || pwUppercase === "YES" || pwUppercase === "yes") {
            //link to generatebtn somehow
@@ -33,11 +33,11 @@ var generateBtn = function() {
         else {
           console.log("no")
         }
-      }
+      //}
     }
 
     var pwLowercase = function() {
-      for (var i = 0; i < pwUppercase.length; i+(i+25)) {
+      //for (var i = 0; i < pwUppercase.length; i+(i+25)) {
         window.prompt("Would you like to include lower case letters? Type 'Yes' or 'No'");
         if (pwLowercase === "Yes" || pwLowercase === "YES" || pwLowercase === "yes") {
           //link to generate button 
@@ -46,28 +46,34 @@ var generateBtn = function() {
         else {
           console.log("no")
         }
-      } 
+      //} 
     }
     //choose if want to add numbers to password
     var numBers = function() {
-      for (var i = 0; i < pwUppercase.length; i+(i+9))
-      window.prompt("Would you like to include numbers? Type 'Yes' or 'No'");
-      if (pwLowercase === "Yes" || pwLowercase === "YES" || pwLowercase === "yes") {
-        console.log("yes");
-      }
-      else {
-        console.log("no")
-      }
+      //for (var i = 0; i < pwUppercase.length; i+(i+9)) {
+        window.prompt("Would you like to include numbers? Type 'Yes' or 'No'");
+        if (pwLowercase === "Yes" || pwLowercase === "YES" || pwLowercase === "yes") {
+          console.log("yes");
+        }
+        else {
+          console.log("no")
+        }
+      //}
     }
 
     var specialCharacters = function() {
-      for (var i = 0; i < pwUppercase.length; i+(i+5))
-      window.prompt("Would you like to include special characters? (i.e. @ $ % etc...) Type 'Yes' or 'No'");
-      if (specialCharacters === "Yes" || specialCharacters === "YES" || specialCharacters === "yes") {
-        console.log("yes")
-      }
+      //for (var i = 0; i < pwUppercase.length; i+(i+5)) {
+        window.prompt("Would you like to include special characters? (i.e. @ $ % etc...) Type 'Yes' or 'No'");
+        if (specialCharacters === "Yes" || specialCharacters === "YES" || specialCharacters === "yes") {
+          console.log("yes")
+        }
+        else {
+          //if they do not choose a special character
+          window.alert("Please choose at least 1 special character!");
+        }
+      //}
     }
-  //}  
+  }  
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
