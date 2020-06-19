7ac97d7b2f0d1ec3bfb008a26ad3bbfc8f1e052d
heatherzen <heather.zen@gmail.com>
2020-06-19 12:54:41 -0700

    changed var strings added function generateBtn

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,18 @@
 // Assignment code here
-var pwlenGth = (pwlenGth >= 8 && pwLenGth <= 128);
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numBers = ["0", "1", "2", "3", "4", "5","6", "7", "8", "9"];
-var specialCharacters = ["U+0021", "U+0023", "U+0024", "U+0025", "U+0026", "U+0040"];
+var lenGth = "";
+var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+var numBers = "0123456789";
+var specialCharacters = "!@#$%&*";
 
 
     
 window.alert("Please choose your password criteria.")
     
+    funtion generateBtn() {
       //password length criteria
-       
-        for (i=0; i < pwlenGth.length; i++) {
-          var pwlenGth = math.floor(math.random() *128 +8)
-          console.log(pwlenGth);
+      var lenGth() {
+        for (var i = 8; i<= 128; i++) {
           window.prompt("Choose the length of your password. (It must be a minimum of 8 charactors and a maximum of 128 characters in length).");
           if (pwLength < 8) {
             window.alert("Please choose a length of at least 8 characters!");
@@ -21,7 +20,9 @@ window.alert("Please choose your password criteria.")
           else if (pwlength > 128) {
             window.alert("Please choose a length no more that 128 characters!")
           }
+          console.log(pwlenGth);
         }
+      }
       
       //verify Uppercase 'yes' or 'no'
       
@@ -29,7 +30,7 @@ window.alert("Please choose your password criteria.")
           var upperCase = math.floor(math.random() *25 +1)
           console.log(upperCase);
           window.prompt("Would you like to include Upper case letters? Type 'Yes' or 'No'");
-          if (pwUppercase === "Yes" || pwUppercase === "YES" || pwUppercase === "yes") {
+          if (upperCase === "Yes" || upperCase === "YES" || upperCase === "yes") {
             //link to generatebtn somehow
             console.log("yes");
           }
@@ -40,11 +41,11 @@ window.alert("Please choose your password criteria.")
       
 
       
-        for (var i = 0; i < lowerCase.length; i+(i+25)) {
+        for (var i = 0; i < lowerCase.length; i++) {
           var lowerCase = math.floor(math.random() *25 +1)
           console.log(lowerCase);
           window.prompt("Would you like to include lower case letters? Type 'Yes' or 'No'");
-          if (pwLowercase === "Yes" || pwLowercase === "YES" || pwLowercase === "yes") {
+          if (lowerCase === "Yes" || lowerCase === "YES" || lowerCase === "yes") {
             //link to generate button 
             console.log("yes");
           }
@@ -59,7 +60,7 @@ window.alert("Please choose your password criteria.")
           var numBers = math.floor(math.random() *10)
           console.log(numBers);
           window.prompt("Would you like to include numbers? Type 'Yes' or 'No'");
-          if (pwLowercase === "Yes" || pwLowercase === "YES" || pwLowercase === "yes") {
+          if (numBers === "Yes" || numBers === "YES" || numBers === "yes") {
             console.log("yes");
           }
           else {
@@ -81,7 +82,7 @@ window.alert("Please choose your password criteria.")
             window.alert("Please choose at least 1 special character!");
           }
         }
-      
+    }  
       
   
 
