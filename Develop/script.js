e4926171eaf816575fca3fee7bf0319ef70ff4f7
heatherzen <heather.zen@gmail.com>
2020-06-19 11:00:14 -0700

    changed for loops added math random

--- Develop/script.js
+++ Develop/script.js
@@ -10,58 +10,68 @@ var specialCharacters = ["U+0021", "U+0023", "U+0024", "U+0025", "U+0026", "U+00
 window.alert("Please choose your password criteria.")
     
       //password length criteria
-      var pwlenGth = function() {
+       
         for (i=0; i < pwlenGth.length; i++) {
+          var pwlenGth = math.floor(math.random() *128 +8)
+          console.log(pwlenGth);
           window.prompt("Choose the length of your password. (It must be a minimum of 8 charactors and a maximum of 128 characters in length).");
           if (pwLength < 8) {
             window.alert("Please choose a length of at least 8 characters!");
           }
-          else if (pwLength > 128) {
+          else if (pwlength > 128) {
             window.alert("Please choose a length no more that 128 characters!")
           }
         }
-      }
+      
       //verify Uppercase 'yes' or 'no'
-      var upperCase = function() {
-        for (var i = 0; i < pwUppercase.length; i+(i+25)) {
+      
+        for (var i = 0; i < upperCase.length; i++) {
+          var upperCase = math.floor(math.random() *25 +1)
+          console.log(upperCase);
           window.prompt("Would you like to include Upper case letters? Type 'Yes' or 'No'");
           if (pwUppercase === "Yes" || pwUppercase === "YES" || pwUppercase === "yes") {
             //link to generatebtn somehow
             console.log("yes");
           }
           else {
-            console.log("no")
+            console.log("no");
           }
         }
-      }
+      
 
-      var lowerCase = function() {
-        for (var i = 0; i < pwUppercase.length; i+(i+25)) {
+      
+        for (var i = 0; i < lowerCase.length; i+(i+25)) {
+          var lowerCase = math.floor(math.random() *25 +1)
+          console.log(lowerCase);
           window.prompt("Would you like to include lower case letters? Type 'Yes' or 'No'");
           if (pwLowercase === "Yes" || pwLowercase === "YES" || pwLowercase === "yes") {
             //link to generate button 
             console.log("yes");
           }
           else {
-            console.log("no")
+            console.log("no");
           }
         } 
-      }
+      
       //choose if want to add numbers to password
-      var numBers = function() {
-        for (var i = 0; i < pwUppercase.length; i+(i+9)) {
+      
+        for (var i = 0; i < numBers.length; i++) {
+          var numBers = math.floor(math.random() *10)
+          console.log(numBers);
           window.prompt("Would you like to include numbers? Type 'Yes' or 'No'");
           if (pwLowercase === "Yes" || pwLowercase === "YES" || pwLowercase === "yes") {
             console.log("yes");
           }
           else {
-            console.log("no")
+            console.log("no");
           }
         }
-      }
+      
 
-      var specialCharacters = function() {
-        for (var i = 0; i < pwUppercase.length; i+(i+5)) {
+      
+        for (var i = 0; i < specialCharacters.length; i++) {
+          var specialCharacters = math.floor(math.random() *6)
+          console.log(specialCharacters);
           window.prompt("Would you like to include special characters? (i.e. @ $ % etc...) Type 'Yes' or 'No'");
           if (specialCharacters === "Yes" || specialCharacters === "YES" || specialCharacters === "yes") {
             console.log("yes")
@@ -71,7 +81,7 @@ window.alert("Please choose your password criteria.")
             window.alert("Please choose at least 1 special character!");
           }
         }
-      }
+      
       
   
 
